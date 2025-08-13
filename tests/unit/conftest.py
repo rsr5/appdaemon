adbadef823d15173b2ff779b0f5c6dfb16422900
John Lancaster <32917998+jsl12@users.noreply.github.com>
2025-08-13 13:05:35 -0500

    linting

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,11 +5,10 @@ from datetime import date, datetime, time
 from functools import partial
 
 import pytest
+from appdaemon import utils
 from astral import LocationInfo
 from astral.location import Location
 from pytz import BaseTzInfo, timezone
-
-from appdaemon import utils
 
 pytestmark = pytest.mark.ci
 
